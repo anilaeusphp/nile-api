4639417ec33d1d13a0ae19d9a8f0bb22ac8b2822
rubyongainz <123082063+rubyongainz@users.noreply.github.com>
2023-10-04 08:18:19 +0000

    rspec for books_controller done

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,38 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      before_action :set_book, only: [:destroy]
+
+      def index
+        render json: Book.all, status: :ok
+      end
+
+      def create
+        book = Book.new(book_params)
+
+        if book.save
+          render json: book, status: :created
+        else
+          render json: book.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        if @book.destroy!
+          render json: "Book has been deleted", status: :no_content
+        end
+      end
+
+      private
+      def book_params
+        params.require(:book).permit(:title, :author)
+      end
+
+      private
+      def set_book
+        @book = Book.find(params[:id])
+      end
+
+    end
+  end
+end
