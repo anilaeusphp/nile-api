8efacf74d190042c94fd88e26444171680fb6386
rubyongainz <123082063+rubyongainz@users.noreply.github.com>
2023-10-04 11:34:53 +0000

    pagination logic added

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,11 +4,13 @@ module Api
       before_action :set_book, only: [:destroy]
 
       def index
-        render json: Book.all, status: :ok
+        books = Book.limit(params[:limit]).offset(params[:offset])
+        render json: BooksRepresenter.new(books).as_json, status: :ok
       end
 
       def create
-        book = Book.new(book_params)
+        author = Author.create!(author_params)
+        book = Book.new(book_params.merge(author_id: author.id))
 
         if book.save
           render json: book, status: :created
@@ -25,7 +27,12 @@ module Api
 
       private
       def book_params
-        params.require(:book).permit(:title, :author)
+        params.require(:book).permit(:title)
+      end
+
+      private
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :age)
       end
 
       private
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates :author, :title, presence: true
   validates :title, length: {minimum: 2, maximum: 150}
+
+  belongs_to :author
 end
